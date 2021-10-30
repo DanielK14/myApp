f340d67b723c257a327a174a094d510b8051316f
Daniel Krois <danielkrois@hotmail.com>
2021-10-30 23:04:16 +0200

    added code for the FizzBuzz project

---
+++ FizzBuzz.js
@@ -0,0 +1,17 @@
+function fizzBuzz(fizz, buzz, runs){
+    /*for (let i=0; i < runs ; i++) {
+        if (i%fizz == 0 && i%buzz == 0){
+            document.write("FizzBuzz");
+        }else if (i%fizz == 0 && i%buzz != 0){
+            document.write("Fizz");
+        }else if (i%fizz != 0 && i%buzz == 0){
+            document.write("Buzz");
+        }else{
+            document.write(i);
+        }
+    }*/
+    console.log("Hello");
+    document.write("hi");
+}
+
+
